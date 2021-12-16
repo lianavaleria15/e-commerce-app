3025a1c7f58779f9b9f9cc61587e89b21e5af24b
liana.valeria15 <liana.valeria15@gmail.com>
2021-12-16 17:15:24 +0000

    added categories has many products association

--- src/models/index.js
+++ src/models/index.js
@@ -8,6 +8,7 @@ const ProductTag = require("./ProductTag");
 Product.belongsTo(Category, { foreignKey: "category_id" });
 
 // Categories have many Products
+Category.hasMany(Product, { foreignKey: "category_id" });
 
 // Products belongToMany Tags (through ProductTag)
 
