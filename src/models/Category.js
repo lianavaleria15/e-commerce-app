8b5168cdbfdad79124ddd817b5e6f51531785650
liana.valeria15 <liana.valeria15@gmail.com>
2021-12-16 12:28:03 +0000

    added given folders and set category model

---
+++ src/models/Category.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+
+//create a new sequelize model for category
+class Category extends Model {}
+
+Category.init(
+  // define columns
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      //doesn't allow null values
+    },
+    category_name: {
+      type: DataTypes.STRING,
+    },
+  },
+
+  //link to db connection
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
+
+module.exports = Category;
