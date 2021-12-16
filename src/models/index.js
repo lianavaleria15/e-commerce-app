87fcace72417bd47884cf8b224f47b566642f639
liana.valeria15 <liana.valeria15@gmail.com>
2021-12-16 17:19:23 +0000

    added belongs to many association for produc and tag models

--- src/models/index.js
+++ src/models/index.js
@@ -11,8 +11,10 @@ Product.belongsTo(Category, { foreignKey: "category_id" });
 Category.hasMany(Product, { foreignKey: "category_id" });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
