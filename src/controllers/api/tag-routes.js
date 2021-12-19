0beeb9b8c776e13e62afbdbf369fa784d0174689
liana.valeria15 <liana.valeria15@gmail.com>
2021-12-19 19:13:37 +0000

    added get all tags and tag by id fs controllers

--- src/controllers/api/tag-routes.js
+++ src/controllers/api/tag-routes.js
@@ -1,10 +1,29 @@
 //import category and product models
 const { Tag, Product, ProductTag } = require("../../models");
 
-const getAllTags = (req, res) => {};
-const getTag = (req, res) => {};
+const getAllTags = async (req, res) => {
+  try {
+    const allTagsData = await Tag.findAll();
+    return res.status(200).json(allTagsData);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
+const getTag = async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id);
+
+    if (!tagData) {
+      res.status(404).json({ message: "No location was found for this id!" });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(err).json(err);
+  }
+};
 const createTag = (req, res) => {};
-const updateTag = (req, res) => {};
-const deleteTag = (req, res) => {};
+const updateTag = async (req, res) => {};
+const deleteTag = async (req, res) => {};
 
 module.exports = { getAllTags, getTag, createTag, updateTag, deleteTag };
