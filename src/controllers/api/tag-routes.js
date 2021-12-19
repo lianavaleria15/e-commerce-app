5ed575cb2e0574fc4104dc3f3d8905acd9694adb
liana.valeria15 <liana.valeria15@gmail.com>
2021-12-19 20:06:28 +0000

    added create, update and delete controller fs

--- src/controllers/api/tag-routes.js
+++ src/controllers/api/tag-routes.js
@@ -22,8 +22,37 @@ const getTag = async (req, res) => {
     res.status(err).json(err);
   }
 };
-const createTag = (req, res) => {};
-const updateTag = async (req, res) => {};
-const deleteTag = async (req, res) => {};
+const createTag = async (req, res) => {
+  try {
+    const { newTagData } = await Tag.create(req.body);
+    res.status(200).json(newTagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+const updateTag = async (req, res) => {
+  try {
+    const { updatedTag } = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(updatedTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+const deleteTag = async (req, res) => {
+  try {
+    const { updatedCategory } = await Tag.destroy(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
 module.exports = { getAllTags, getTag, createTag, updateTag, deleteTag };
